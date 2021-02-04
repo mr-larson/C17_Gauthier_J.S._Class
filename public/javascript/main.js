aca8e42a4ec3e3e4a0650e71cabffc478f95d317
GAUTHIER <gauthierdewit@gmail.com>
2021-02-04 13:43:04 +0100

    vierge

---
+++ public/javascript/main.js
@@ -0,0 +1,109 @@
+// Les Classes
+//-----------------------------
+//Présentation
+/* 
+class Ingredients {
+    constructor (nom, poids, prix, couleur){
+        this.nom = nom;
+        this.poids = poids;
+        this.prix = prix;
+        this.couleur = couleur;
+        this.acheter = (personne) => {
+            if (personne == null) {
+                console.log (`Quelqu'un m'a acheté`)
+            } else {
+                console.log (`${personne} m'a acheté`)
+            }
+        }
+    }
+}
+
+let tomate = new Ingredients ("Tomate", 0.5, 0.7, "Aldi", "Rouge")
+tomate.acheter ("Elias")
+console.log(`Le ${tomate.nom} coute ${tomate.prix} €`)
+ */
+//-------------------------------
+// EXO 1.1
+/* 
+class Coding{
+    constructor (age, nom, ville){
+        this.age = age
+        this.nom = nom
+        this.ville = ville
+    }
+}
+let gauthier = new Coding (35, "Gauthier", "Waterloo")
+let cem = new Coding (22, "Cem", "forest")
+ */
+//--------------------------------
+// 1.2
+/* 
+class Coding{
+    constructor (age, nom, ville){
+        this.age = age
+        this.nom = nom
+        this.ville = ville
+        this.sePresenter = (x) =>{
+            console.log (`Bonjour je m'appelle ${x} !`)
+        }
+    }
+}
+let Gauthier = new Coding (35, "Gauthier", "Waterloo")
+let Cem = new Coding (22, "Cem", "forest")
+
+Gauthier.sePresenter (Gauthier.nom) 
+ */
+//--------------------------------
+// EXO 2.1
+
+class Lieux{
+    constructor (nom, contenu, ingredients){
+        this.nom = nom
+        this.contenu = contenu
+        this.ingredients = ingredients
+    }
+}
+let maison = new Lieux ("Maison", [])
+let epicerie = new Lieux ("Epicerie", [], [])
+let cuisine = new Lieux ("Cuisine", [], [])
+
+class Ingredients{
+    constructor (nom, etat, prix){
+        this.nom = nom
+        this.etat = etat
+        this.prix = prix
+    }
+}
+let poivron = new Ingredients ("poivron", "entier", 1)
+let oignon = new Ingredients ("oignon", "entier", 2)
+let oeuf = new Ingredients ("oeuf", "entier", 4)
+let epice = new Ingredients ("epice", "moulu", 3.25)
+let paprika = new Ingredients ("paprika", "moulu", 1.5)
+let fromage = new Ingredients ("fromage", "coupé", 1.6)
+
+class Personne{
+    constructor (nom, lieu, argent, panier,)
+}
+let maxime = new Personne ("maxime", "néant", 100, [])
+
+//----------------------------
+//EXO 3.1
+
+class Objet{
+    constructor (nom, prix){
+        this.nom = nom
+        this.prix = prix
+    }
+}
+let Marteau = new Objet ("Marteau", 5)
+let Clous = new Objet ("Clous", 1)
+let Tableau = [Marteau, Clous]
+
+class Personnage {
+    constructor (nom, sac, argent){
+        this.nom = nom
+        this.sac = sac
+        this.argent = argent
+    }
+}
+let 
